ed453e38a6cc4d52849d864f2b79f4bbb7f89799
Matthieu Borgognon <matrash@bqn.ch>
2018-08-31 10:01:07 +0200

    Added a PostListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBlog';
+  post: {
+    title: string,
+    content: string,
+    loveIts: number,
+    created_at: Date
+  } 
 }
